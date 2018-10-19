b7914222295362be5908c6603631dc5823591785
erandis-vol <jack.bulan@gmail.com>
2018-10-18 21:28:39 -0400

    Remove unused references

--- GBAHL/Drawing/Color2.KnownColors.cs
+++ GBAHL/Drawing/Color2.KnownColors.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace GBAHL.Drawing
+﻿namespace GBAHL.Drawing
 {
     partial struct Color2
     {
