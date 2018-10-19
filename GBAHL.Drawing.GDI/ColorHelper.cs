1aa549027f09853fc19ddabcc11b25e26537f890
erandis-vol <jack.bulan@gmail.com>
2018-10-18 21:52:00 -0400

    Add GDI project

---
+++ GBAHL.Drawing.GDI/ColorHelper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Drawing;
+
+namespace GBAHL.Drawing.GDI
+{
+    /// <summary>
+    /// Provides methods for colors.
+    /// </summary>
+    public static class ColorHelper
+    {
+        /// <summary>
+        /// Converts this <see cref="Color2"/> instance to the equivalent <see cref="Color"/> instance.
+        /// </summary>
+        /// <param name="color"></param>
+        /// <returns></returns>
+        public static Color ToColor(this Color2 color)
+        {
+            return Color.FromArgb(color.R << 3, color.G << 3, color.B << 3);
+        }
+
+        /// <summary>
+        /// Converts this <see cref="Color"/> instance to the equivalent <see cref="Color2"/> instance.
+        /// </summary>
+        /// <param name="color"></param>
+        /// <returns></returns>
+        public static Color2 ToColor2(this Color color)
+        {
+            return Color2.FromArgb(color.R, color.G, color.B);
+        }
+    }
+}
---
+++ GBAHL.Drawing.GDI/ImageHelper.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+
+namespace GBAHL.Drawing.GDI
+{
+    public static class ImageHelper
+    {
+        /// <summary>
+        /// Returns a palette for this <see cref="Image"/> instance.
+        /// </summary>
+        /// <param name="image">The image.</param>
+        /// <returns></returns>
+        /// <exception cref="FormatException">the image is not indexed.</exception>
+        public static Palette GetPalette(this Image image)
+        {
+            // Make sure the image actually has a palette to grab
+            if (!IsIndexed(image))
+            {
+                throw new FormatException("The image is not indexed.");
+            }
+
+            // Create the palette, converting the colors
+            return new Palette(image.Palette.Entries.Select(x => x.ToColor2()));
+        }
+
+        /// <summary>
+        /// Determines whether this <see cref="Image"/> is indexed.
+        /// </summary>
+        /// <param name="image">The image.</param>
+        /// <returns>true if the image is indexed; otherwise, false.</returns>
+        public static bool IsIndexed(this Image image)
+        {
+            if (image == null)
+                return false;
+
+            return (image.PixelFormat & PixelFormat.Indexed) == PixelFormat.Indexed;
+        }
+    }
+}
