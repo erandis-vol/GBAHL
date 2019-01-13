aeb78866c88162503123c5f8e46f887588778662
erandis-vol <jack.bulan@gmail.com>
2019-01-13 00:42:04 -0500

    Update Tile
    It should be more efficient now

--- GBAHL/Drawing/Tile.cs
+++ GBAHL/Drawing/Tile.cs
@@ -1,53 +1,122 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
 
 namespace GBAHL.Drawing
 {
-    public struct Tile
+    /// <summary>
+    /// Represents an 8x8 array of pixel data.
+    /// </summary>
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    public struct Tile : IEquatable<Tile>
     {
-        private byte[] pixels;
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="Tile"/> struct by copying pixels from the specified array.
-        /// </summary>
-        /// <param name="pixels">The pixels to be copied.</param>
-        /// <exception cref="ArgumentException"><paramref name="pixels"/> does not contain 64 elements.</exception>
-        /// <exception cref="ArgumentNullException"><paramref name="pixels"/> is null.</exception>
-        public Tile(byte[] pixels)
+        [StructLayout(LayoutKind.Sequential, Pack = 1)]
+        private struct Row
         {
-            if (pixels == null)
-                throw new ArgumentNullException(nameof(pixels));
+            private byte p0;
+            private byte p1;
+            private byte p2;
+            private byte p3;
+            private byte p4;
+            private byte p5;
+            private byte p6;
+            private byte p7;
 
-            if (pixels.Length != 64)
-                throw new ArgumentException("Expected 64 pixel elements.", nameof(pixels));
+            public byte this[int index]
+            {
+                get
+                {
+                    switch (index)
+                    {
+                        case 0: return p0;
+                        case 1: return p1;
+                        case 2: return p2;
+                        case 3: return p3;
+                        case 4: return p4;
+                        case 5: return p5;
+                        case 6: return p6;
+                        case 7: return p7;
 
-            this.pixels = (byte[])pixels.Clone();
+                        default:
+                            throw new IndexOutOfRangeException();
+                    }
+                }
+
+                set
+                {
+                    switch (index)
+                    {
+                        case 0: p0 = value; break;
+                        case 1: p1 = value; break;
+                        case 2: p2 = value; break;
+                        case 3: p3 = value; break;
+                        case 4: p4 = value; break;
+                        case 5: p5 = value; break;
+                        case 6: p6 = value; break;
+                        case 7: p7 = value; break;
+
+                        default:
+                            throw new IndexOutOfRangeException();
+                    }
+                }
+            }
+
+            public bool Equals(Row other)
+            {
+                return p0 == other.p0
+                    && p1 == other.p1
+                    && p2 == other.p2
+                    && p3 == other.p3
+                    && p4 == other.p4
+                    && p5 == other.p5
+                    && p6 == other.p6
+                    && p7 == other.p7;
+            }
+
+            public override bool Equals(object obj)
+            {
+                return (obj is Row) && Equals((Row)obj);
+            }
+
+            public override int GetHashCode()
+            {
+                unchecked
+                {
+                    return p1.GetHashCode() ^ p2.GetHashCode() ^ p3.GetHashCode()
+                         ^ p4.GetHashCode() ^ p5.GetHashCode() ^ p6.GetHashCode()
+                         ^ p7.GetHashCode();
+                }
+            }
+
+            public static bool operator ==(Row r1, Row r2)
+            {
+                return r1.Equals(r2);
+            }
+
+            public static bool operator !=(Row r1, Row r2)
+            {
+                return !(r1 == r2);
+            }
         }
+
+        private Row row0;
+        private Row row1;
+        private Row row2;
+        private Row row3;
+        private Row row4;
+        private Row row5;
+        private Row row6;
+        private Row row7;
 
         /// <summary>
         /// Gets or sets the specified pixel.
         /// </summary>
         /// <param name="index">The index of the pixel.</param>
-        /// <returns></returns>
+        /// <exception cref="IndexOutOfRangeException"></exception>
         public byte this[int index]
         {
-            get
-            {
-                if (pixels == null)
-                {
-                    return byte.MinValue;
-                }
-
-                return pixels[index];
-            }
-            set
-            {
-                if (pixels == null)
-                {
-                    pixels = new byte[64];
-                }
-
-                pixels[index] = value;
-            }
+            get => this[index % 8, index / 8];
+            set => this[index % 8, index / 8] = value;
         }
 
         /// <summary>
@@ -55,61 +124,125 @@ namespace GBAHL.Drawing
         /// </summary>
         /// <param name="x">The x-coordinate of the pixel.</param>
         /// <param name="y">The y-coordinate of the pixel.</param>
-        /// <returns></returns>
+        /// <exception cref="IndexOutOfRangeException"></exception>
         public byte this[int x, int y]
         {
-            get => this[x + y * 8];
-            set => this[x + y * 8] = value;
+            get
+            {
+                switch (y)
+                {
+                    case 0: return row0[x];
+                    case 1: return row1[x];
+                    case 2: return row2[x];
+                    case 3: return row3[x];
+                    case 4: return row4[x];
+                    case 5: return row5[x];
+                    case 6: return row6[x];
+                    case 7: return row7[x];
+
+                    default:
+                        throw new IndexOutOfRangeException();
+                }
+            }
+            set
+            {
+                switch (y)
+                {
+                    case 0: row0[x] = value; break;
+                    case 1: row1[x] = value; break;
+                    case 2: row2[x] = value; break;
+                    case 3: row3[x] = value; break;
+                    case 4: row4[x] = value; break;
+                    case 5: row5[x] = value; break;
+                    case 6: row6[x] = value; break;
+                    case 7: row7[x] = value; break;
+
+                    default:
+                        throw new IndexOutOfRangeException();
+                }
+            }
         }
 
-        public bool Equals(ref Tile other, bool flipX, bool flipY)
+        /// <summary>
+        /// Determines whether this <see cref="Tile"/> is equivalent to the specified
+        /// <see cref="Tile"/> value with the specified flipping.
+        /// </summary>
+        /// <param name="other">The tile to compare to.</param>
+        /// <param name="flipX">Determines whether <paramref name="other"/> is to be flipped from left to right.</param>
+        /// <param name="flipY">Determines whether <paramref name="other"/> is to be flipped from top to bottom.</param>
+        /// <returns><c>true</c> if the tiles are equivalent; otherwise, <c>false</c>.</returns>
+        public bool Equals(Tile other, bool flipX = false, bool flipY = false)
         {
             if (flipX || flipY)
             {
-                // TODO: Is the following faster?
-                //fixed (byte* src = &pixels[0])
-                //fixed (byte* dst = &other.pixels[0])
-                //{
-                //    for (int srcY = 0; srcY < 8; srcY++)
-                //    {
-                //        for (int srcX = 0; srcX < 8; srcX++)
-                //        {
-                //            var dstX = flipX ? (7 - srcX) : srcX;
-                //            var dstY = flipY ? (7 - srcY) : srcY;
-                //            if (src[srcX + srcY * 8] != dst[dstX + dstY * 8])
-                //                return false;
-                //        }
-                //    }
-                //}
-
-                for (int y = 0; y < 8; y++)
+                for (int srcY = 0; srcY < 8; srcY++)
                 {
-                    for (int x = 0; x < 8; x++)
+                    for (int srcX = 0; srcX < 8; srcX++)
                     {
-                        var otherX = flipX ? (7 - x) : x;
-                        var otherY = flipY ? (7 - y) : y;
+                        var dstX = flipX ? (7 - srcX) : srcX;
+                        var dstY = flipY ? (7 - srcY) : srcY;
 
-                        if (this[x, y] != other[otherX, otherY])
+                        if (this[srcX, srcY] != other[dstX, dstY])
                         {
                             return false;
                         }
                     }
                 }
+
+                return true;
             }
             else
             {
-                for (int i = 0; i < 64; i++)
-                {
-                    if (this[i] != other[i]) return false;
-                }
+                return row0 == other.row0
+                    && row1 == other.row1
+                    && row2 == other.row2
+                    && row3 == other.row3
+                    && row4 == other.row4
+                    && row5 == other.row5
+                    && row6 == other.row6
+                    && row7 == other.row7;
             }
-
-            return true;
         }
 
-        public bool Equals(ref Tile other)
+        /// <summary>
+        /// Determines whether this <see cref="Tile"/> is equivalent to the specified <see cref="Tile"/>.
+        /// </summary>
+        /// <param name="other">The tile to compare to.</param>
+        /// <returns></returns>
+        public bool Equals(Tile other)
         {
-            return Equals(ref other, false, false);
+            return Equals(other, false, false);
+        }
+
+        /// <summary>
+        /// Determines whether this <see cref="Tile"/> is equivalent to the specified object.
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public override bool Equals(object obj)
+        {
+            return (obj is Tile) && Equals((Tile)obj);
+        }
+
+        /// <summary>
+        /// Returns the hash code for this <see cref="Tile"/>.
+        /// </summary>
+        /// <returns></returns>
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                var hashCode = -1582387468;
+                hashCode = hashCode * -1521134295 + row0.GetHashCode();
+                hashCode = hashCode * -1521134295 + row1.GetHashCode();
+                hashCode = hashCode * -1521134295 + row2.GetHashCode();
+                hashCode = hashCode * -1521134295 + row3.GetHashCode();
+                hashCode = hashCode * -1521134295 + row4.GetHashCode();
+                hashCode = hashCode * -1521134295 + row5.GetHashCode();
+                hashCode = hashCode * -1521134295 + row6.GetHashCode();
+                hashCode = hashCode * -1521134295 + row7.GetHashCode();
+                return hashCode;
+            }
         }
     }
 }
