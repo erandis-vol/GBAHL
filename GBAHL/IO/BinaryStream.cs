ac6c590469325ba00fe9f948cf590147cb6a3e10
erandis-vol <jack.bulan@gmail.com>
2019-01-16 13:37:14 -0500

    Improve Find methods

--- GBAHL/IO/BinaryStream.cs
+++ GBAHL/IO/BinaryStream.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
 using System.Text;
 
 namespace GBAHL.IO
@@ -453,16 +454,55 @@ namespace GBAHL.IO
         }
         */
 
-        public int Find(byte search, int count, int startOffset = 0)
+        public int Find(byte search, int count, int from = 0, int alignment = 1)
         {
-            if (startOffset < 0 || startOffset > stream.Length - count)
-                return -1;
+            if (from < 0 || from >= stream.Length - count)
+                throw new ArgumentException("", nameof(from));
 
-            if (count <= 0)
-                return startOffset;
+            if (count <= 0 || count > stream.Length)
+                throw new ArgumentException("", nameof(search));
 
+            if (alignment <= 0)
+                alignment = 1;
+
+            if (from % alignment != 0)
+                from -= from % alignment;
+
+            int streamPosition = (int)stream.Position;
+            int i = from;
+            int j = -1;
+            byte[] buffer = new byte[count];
+
+            while (i < stream.Length)
+            {
+                stream.Position = i;
+
+                // Update the search buffer
+                FillSearchBuffer(buffer);
+
+                // Check for a match
+                bool match = true;
+                foreach (byte b in buffer)
+                {
+                    if (b != search)
+                    {
+                        match = false;
+                        break;
+                    }
+                }
+
+                if (match)
+                {
+                    j = i;
+                    break;
+                }
+
+                i += alignment;
+            }
+
+            /*
             byte[] buffer = ReadAllBytes();
-            for (int i = startOffset; i < buffer.Length - count; i++)
+            for (int i = from; i < buffer.Length - count; i++)
             {
                 int j = 0;
                 while (buffer[i + j] == search && j < count)
@@ -475,10 +515,13 @@ namespace GBAHL.IO
                     return i;
                 }
             }
+            */
 
-            return -1;
+            stream.Position = streamPosition;
+            return j;
         }
 
+        /*
         public int Find(byte[] search, int startOffset = 0)
         {
             if (startOffset >= stream.Length - search.Length)
@@ -514,6 +557,95 @@ namespace GBAHL.IO
 
             stream.Position = pos;
             return match;
+        }
+        */
+
+        public int Find(byte[] search, int from = 0, int alignment = 1)
+        {
+            if (search == null)
+                throw new ArgumentNullException(nameof(search));
+
+            if (search.Length > stream.Length)
+                throw new ArgumentException("", nameof(search));
+
+            if (from < 0 || from >= stream.Length - search.Length)
+                throw new ArgumentException("", nameof(from));
+
+            if (alignment <= 0)
+                alignment = 1;
+
+            if (from % alignment != 0)
+                from -= from % alignment;
+
+            int streamPosition = (int)stream.Position;
+            int i = from;
+            int j = -1;
+            byte[] buffer = new byte[search.Length];
+
+            while (i < stream.Length - search.Length)
+            {
+                stream.Position = i;
+
+                // Update the search buffer.
+                FillSearchBuffer(buffer);
+
+                // Check for a match.
+                if (UnsafeCompare(search, buffer))
+                {
+                    j = i;
+                    break;
+                }
+
+                i += alignment;
+            }
+
+            stream.Position = streamPosition;
+            return j;
+        }
+
+        private void FillSearchBuffer(byte[] buffer)
+        {
+            if (buffer.Length == 1)
+            {
+                int n = stream.ReadByte();
+                if (n == -1)
+                    throw new EndOfStreamException();
+
+                buffer[0] = (byte)n;
+            }
+            else
+            {
+                int bytesRead = 0;
+
+                do
+                {
+                    int n = stream.Read(buffer, bytesRead, buffer.Length - bytesRead);
+                    if (n == 0)
+                        throw new EndOfStreamException();
+                    bytesRead += n;
+                } while (bytesRead < buffer.Length);
+            }
+        }
+
+        // Copyright (c) 2008-2013 Hafthor Stefansson
+        // Distributed under the MIT/X11 software license
+        // Ref: http://www.opensource.org/licenses/mit-license.php.
+        private static unsafe bool UnsafeCompare(byte[] a1, byte[] a2)
+        {
+            if (a1 == a2) return true;
+            if (a1 == null || a2 == null || a1.Length != a2.Length)
+                return false;
+            fixed (byte* p1 = a1, p2 = a2)
+            {
+                byte* x1 = p1, x2 = p2;
+                int l = a1.Length;
+                for (int i = 0; i < l / 8; i++, x1 += 8, x2 += 8)
+                    if (*((long*)x1) != *((long*)x2)) return false;
+                if ((l & 4) != 0) { if (*((int*)x1) != *((int*)x2)) return false; x1 += 4; x2 += 4; }
+                if ((l & 2) != 0) { if (*((short*)x1) != *((short*)x2)) return false; x1 += 2; x2 += 2; }
+                if ((l & 1) != 0) if (*((byte*)x1) != *((byte*)x2)) return false;
+                return true;
+            }
         }
 
         #endregion
