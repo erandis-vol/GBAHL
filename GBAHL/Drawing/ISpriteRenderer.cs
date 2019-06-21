aa791cb5ad1974b6b5b5ab42d5eba28840de2e00
erandis-vol <jack.bulan@gmail.com>
2019-06-20 21:35:45 -0400

    Create ISpriteRenderer

---
+++ GBAHL/Drawing/ISpriteRenderer.cs
@@ -0,0 +1,16 @@
+﻿namespace GBAHL.Drawing
+{
+    /// <summary>
+    /// Provides a mechanism for rendering a sprite as an image.
+    /// </summary>
+    /// <typeparam name="TImage"></typeparam>
+    public interface ISpriteRenderer<TImage>
+    {
+        /// <summary>
+        /// Draws the sprite as an image.
+        /// </summary>
+        /// <param name="sprite">The sprite to be drawn.</param>
+        /// <returns></returns>
+        TImage Draw(Sprite sprite);
+    }
+}
